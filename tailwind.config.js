3512cca83aeac8b2b30b3d2bc5bf05d14690facb
aldanfatur <aldan.fatu@student.umn.ac.id>
2023-06-10 18:24:21 +0700

    Content Creating 101

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sg-blue": "#153C8A",
+        "sg-sky-blue": "#91C5F1",
+        "sg-yellow": "#FECE2F",
+        "sg-red": "#C62700",
+      },
+      fontFamily: {
+        ubuntu: ["Ubuntu", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
