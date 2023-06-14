3243392cd64468d4dd93c2cd8322eefa7d079aa6
aldanfatur <aldan.fatu@student.umn.ac.id>
2023-06-14 10:35:52 +0700

    Sahabat Gempa Website V1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       colors: {
@@ -19,5 +19,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("./node_modules/flowbite")],
 };
